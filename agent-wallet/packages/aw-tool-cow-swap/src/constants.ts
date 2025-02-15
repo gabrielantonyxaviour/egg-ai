cffd730af8b7eda6538fd0e5bf6d60223007c3b9
Gabriel <gabrielantony56@gmail.com>
2025-02-16 03:01:32 +0530

    arbitrum constants

---
+++ agent-wallet/packages/aw-tool-cow-swap/src/constants.ts
@@ -0,0 +1,32 @@
+export const constants = {
+    [11155111]: {
+        "name": "Sepolia Testnet",
+        "assets": {
+            "all": [
+                "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14",
+                "0x0625aFB445C3B6B7B929342a04A22599fd5dBB59",
+                "0xbe72E441BF55620febc26715db68d3494213D8Cb",
+                "0x58Eb19eF91e8A6327FEd391b51aE1887b833cc91"
+            ],
+            "weth": "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14",
+            "cow": "0x0625aFB445C3B6B7B929342a04A22599fd5dBB59",
+            "usdc": "0xbe72E441BF55620febc26715db68d3494213D8Cb",
+            "usdt": "0x58Eb19eF91e8A6327FEd391b51aE1887b833cc91",
+        }
+    },
+    [42161]: {
+        "name": "Arbitrum",
+        "assets": {
+            "all": [
+                "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+                "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+                "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+                "0xcb8b5CD20BdCaea9a010aC1F8d835824F5C87A04"
+            ],
+            "weth": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+            "cow": "0xcb8b5CD20BdCaea9a010aC1F8d835824F5C87A04",
+            "usdc": "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+            "usdt": "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+        }
+    }
+}
